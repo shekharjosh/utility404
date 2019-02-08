fda6f13abaa3d91440be392b6fb71b78700577db
Chandra Shekhar 4 <cshekhar3@sapient.com>
2019-02-08 11:42:19 +0000

    execute the url with the batch

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ## This utility is made to find out the 404 error pages and images greater then 100 kb on the site. Steps to follow to run this utility.
 
 1. Download the package from git.
-2. Run npm install.
-3. After installing the node packages, open request.js file and put the sitemap.xml path for 'inputUrl' variable.
-4. On terminal run command npm start.
-5. On complete two files save one for 404 error (statusFile.txt) and other for images (imageFile.txt)
+2. Run - npm i
+3. On terminal run command - npm start <site_map_xml_url> [<min_image_size_in_KB (Default to 5000Bytes)>] [<viewport_width_in_digit (Default to 1300)> ] [<viewport_hight_in_digit (Default to 900)>]
+4. On complete two files are saved. One for 404 error (statusFile.txt) and other for images (imageFile.csv)
--- request.js
+++ request.js
@@ -1,82 +1,125 @@
-var https = require('https'),
+const BATCH_SIZE = 10,
+    DELAY = 1000;
+
+let https = require('https'),
     fs = require('fs'),
     flow = require('xml-flow'),
     puppeteer = require('puppeteer'),
-    inputUrl = 'https://www.lipton.com/ru/sitemap.xml',
-    file = fs.createWriteStream('sitemap.xml');   
+    inputUrl = process.argv[2] || process.exit(-1),
+    fileSize = process.argv[3] || 100000,
+    viewportWidth = process.argv[4] || 1300,
+    viewportHeight = process.argv[5] || 900;
 
-https.get(inputUrl, function(res) {
-    res.on('data', function(xmlData) {
-        file.write(xmlData);
-    }).on('end', function() {
-        file.end();
-        
-        var inFile = fs.createReadStream('sitemap.xml'),
-            xmlStream = flow(inFile),
-            array = [],
-            statusTypeSet = new Set(),
-            imageTypeSet = new Set();
-            
-            xmlStream.on('tag:loc', function(url){
-                array.push(url.$text);              
-            }).on('end', async function(){
-                try {
-                    var browser = await puppeteer.launch(),
-                        urlRead = array.map(async function(url, i){
-                            var page = await browser.newPage();
-                            
-                            process.on("unhandledRejection", function(reason, p) {
-                              console.error("Unhandled Rejection at: Promise", p, "reason:", reason);
-                              browser.close();
-                            });
-                            
-                            await page.setRequestInterception(true);
-                                
-                            page.on('request', function(request) {
-                                request.continue();
-                            });
-                            
-                            page.on('response', function(response){
-                                if (response.status() === 404) {
-                                    statusTypeSet.add(response.url());
-                                }
-                                
-                                if(response.request().resourceType() === 'image') {
-                                    response.buffer().then(buffer => {
-                                        if(buffer.length > 100000) {
-                                            imageTypeSet.add(response.url() + ' ( Size: ' + buffer.length/1000 + ' kb )');
-                                        }
-                                    }, error => {
-                                        console.log(error)
-                                    });
-                                }
-                            });
+fs.unlink('sitemap.xml', (error) => {
+    if (error) console.log('')
+});
+fs.unlink('statusFile.txt', (error) => {
+    if (error) console.log('')
+});
+fs.unlink('imageFile.csv', (error) => {
+    if (error) console.log('')
+});
 
-                            console.log(`loading page: ${url}`);
-                            //await page.authenticate({username:"fp", password:"dove"});
-                            await page.goto(url, {timeout: 0});
-                            
-                            console.log(`closing page: ${url}`);
-                            await page.close();
-                        });
-                    
-                    Promise.all(urlRead).then(function() {
-                        fs.writeFile('statusFile.txt', Array.from(statusTypeSet).join('\n'), function(err) {
-                            if(err) throw err;
-                            console.log('Status file saved successfully');
-                      });
-                      
-                      fs.writeFile('imageFile.txt', Array.from(imageTypeSet).join('\n'), function(err) {
-                                if(err) throw err;
-                                console.log('Image file saved successfully');
-                        });
-                      
-                      browser.close();
-                    });
-                } catch(e) {
-                    console.log(e);
-                    browser.close();
-                }   
+let splitIntoSubArray = (arr, count = BATCH_SIZE) => {
+    var newArray = [];
+    while (arr.length > 0) {
+        newArray.push(arr.splice(0, count));
+    }
+    return newArray;
+}
+
+let delay = (time, data) => new Promise(resolve => {
+    setTimeout(resolve.bind(null, data), time);
+});
+
+https.get(inputUrl, res => {
+    let siteMapFile = fs.createWriteStream('sitemap.xml'),
+        array = [];
+    res.on('data', xmlData => {
+        siteMapFile.write(xmlData);
+    }).on('end', () => {
+        siteMapFile.end();
+        siteMapFile = fs.createReadStream('sitemap.xml');
+        flow(siteMapFile).on('tag:loc', url => {
+            array.push(url.$text);
+        }).on('end', async () => {
+            runBatchCapture(array).then(() => {
+                console.log('>>>>> All data Captured.');
+            }).catch(error => {
+                console.error('>>>>> Error Occurred - ' + error);
             });
+        });
     });
 });
+
+let runBatchCapture = async (array) => {
+    let newArray = splitIntoSubArray(array),
+        index = 0,
+        next = () => {
+            if (index < newArray.length) {
+                return crawlPages(newArray[index++]).then(async function () {
+                    return delay(DELAY).then(next);
+                });
+            }
+        }
+    return Promise.resolve().then(next);
+}
+
+let crawlPages = async (array) => {
+    try {
+        let statusTypeSet = new Set(),
+            imageTypeSet = new Set(),
+            browser = await puppeteer.launch(),
+            urlRead = array.map(async (url) => {
+                let page = await browser.newPage();
+                page.viewport({
+                    width: viewportWidth,
+                    height: viewportHeight
+                });
+                process.on("unhandledRejection", (reason, promise) => {
+                    console.error("Unhandled Rejection at: Promise", promise, "reason:", reason);
+                    browser.close();
+                });
+                await page.setRequestInterception(true);
+                page.on('request', request => {
+                    request.continue();
+                });
+                page.on('response', response => {
+                    if (response.status() === 404) {
+                        statusTypeSet.add(response.url());
+                    }
+                    if (response.request().resourceType() === 'image') {
+                        response.buffer().then(buffer => {
+                            if (buffer.length > fileSize) {
+                                imageTypeSet.add(`\n${url}, ${response.url()}, ${buffer.length / 1000 + ' KB'}`);
+                            }
+                        }, error => {
+                            console.log('>>>>> Error Occurred - ' + error);
+                        });
+                    }
+                });
+                console.log(`Loading page: ${url}`);
+                //await page.authenticate({username:"fp", password:"dove"});
+                await page.goto(url, {
+                    timeout: 0
+                });
+                console.log(`Closing page: ${url}`);
+                await page.close();
+            });
+
+        Promise.all(urlRead).then(function () {
+            fs.appendFile('statusFile.txt', Array.from(statusTypeSet).join('\n'), error => {
+                if (error) throw error;
+                console.log('Status file appended successfully.');
+            });
+            fs.appendFile('imageFile.csv', Array.from(imageTypeSet).join('\n'), error => {
+                if (error) throw error;
+                console.log('Image data appended successfully.');
+            });
+            browser.close();
+        });
+    } catch (error) {
+        console.log('>>>>> Error Occurred - ' + error);
+        browser.close();
+    }
+}
